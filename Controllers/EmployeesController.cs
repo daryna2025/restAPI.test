548eca83a517968eb6eee777b6a20ec1162171fd
Daryna Sorokina <daryna.sorokina@student.careeria.fi>
2026-03-24 21:54:29 +0200

    GET kaikki Employees

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using restAPI.Models;
 
 namespace restAPI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class EmployeesController : Controller
     {
-        public IActionResult Index()
+        //------dependency injection tapa------
+        private NorthwindOriginalContext db;
+
+        public EmployeesController(NorthwindOriginalContext dbparametri)
         {
-            return View();
+            db = dbparametri;
         }
+
+        //hakee kaikki työntekijät tietokannasta ja palauttaa ne JSON-muodossa
+        [HttpGet]
+
+        public ActionResult GetAllEmployees()
+        {
+            try
+            {
+                var työntekijä = db.Employees.ToList();
+                return Json(työntekijä);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
+            }
+
+        }
+
     }
 }
